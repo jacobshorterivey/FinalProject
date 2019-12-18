d902db4e3cc46d81ba918389e12f786ddfe7f7c7
Samantha Haviland <shaviland@live.com>
2019-12-18 10:38:50 -0700

    I'm a ditz

--- JPAFurever/src/test/java/com/skilldistillery/furever/entities/Species.java
+++ JPAFurever/src/test/java/com/skilldistillery/furever/entities/Species.java
@@ -1,34 +1,52 @@
 package com.skilldistillery.furever.entities;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class Species {
+	private static EntityManagerFactory emf;
+	private static EntityManager em;
+	private Shelter shelter;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("Furever");
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
+		emf.close();
 	}
 
 	@BeforeEach
 	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		shelter = em.find(Shelter.class, 1);
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		shelter = null;
+		em.close();
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	@DisplayName("Testing Entity mapping")
+	void name() {
+		assertNotNull(shelter);
+		assertEquals("Dog", shelter.getName());
 	}
 
 }
