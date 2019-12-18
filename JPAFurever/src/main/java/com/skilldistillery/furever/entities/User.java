f4f8aeaa237ff7c9adc4e572444666ccc47719ef
Samantha Haviland <shaviland@live.com>
2019-12-18 09:49:19 -0700

    User entity done

--- JPAFurever/src/main/java/com/skilldistillery/furever/entities/User.java
+++ JPAFurever/src/main/java/com/skilldistillery/furever/entities/User.java
@@ -1,28 +1,51 @@
 package com.skilldistillery.furever.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
 public class User {
+
 	// FIELDS
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String username;
+	private String password;
+	private String fname;
+	private String lname;
+	private Integer age;
+	private String phone;
+	private boolean active;
+	@Column(name="address_id")
+	private Integer addressId;
 	
 	// CONSTRUCTORS
-	public User(int id, String username) {
-		super();
-		this.id = id;
-		this.username = username;
-	}
-	
 	public User() {
 		super();
 	}
 
+	public User(int id, String username, String password, String fname, String lname, Integer age, String phone,
+			boolean active, Integer addressId) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.fname = fname;
+		this.lname = lname;
+		this.age = age;
+		this.phone = phone;
+		this.active = active;
+		this.addressId = addressId;
+	}
+
+
 	// GETTERS, SETTERS, TOSTRING, EQUALS
+
+
 	public int getId() {
 		return id;
 	}
@@ -39,12 +62,134 @@ public class User {
 		this.username = username;
 	}
 
-	@Override
-	public String toString() {
-		StringBuilder builder = new StringBuilder();
-		builder.append("User [id=").append(id).append(", username=").append(username).append("]");
-		return builder.toString();
+	public String getPassword() {
+		return password;
 	}
 
-	// METHODS
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFname() {
+		return fname;
+	}
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+	public String getLname() {
+		return lname;
+	}
+
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public Integer getAddressId() {
+		return addressId;
+	}
+
+	public void setAddressId(Integer addressId) {
+		this.addressId = addressId;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (active ? 1231 : 1237);
+		result = prime * result + ((addressId == null) ? 0 : addressId.hashCode());
+		result = prime * result + ((age == null) ? 0 : age.hashCode());
+		result = prime * result + ((fname == null) ? 0 : fname.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((lname == null) ? 0 : lname.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((phone == null) ? 0 : phone.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (active != other.active)
+			return false;
+		if (addressId == null) {
+			if (other.addressId != null)
+				return false;
+		} else if (!addressId.equals(other.addressId))
+			return false;
+		if (age == null) {
+			if (other.age != null)
+				return false;
+		} else if (!age.equals(other.age))
+			return false;
+		if (fname == null) {
+			if (other.fname != null)
+				return false;
+		} else if (!fname.equals(other.fname))
+			return false;
+		if (id != other.id)
+			return false;
+		if (lname == null) {
+			if (other.lname != null)
+				return false;
+		} else if (!lname.equals(other.lname))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (phone == null) {
+			if (other.phone != null)
+				return false;
+		} else if (!phone.equals(other.phone))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", password=" + password + ", fname=" + fname + ", lname="
+				+ lname + ", age=" + age + ", phone=" + phone + ", active=" + active + ", addressId=" + addressId + "]";
+	}
+
+	
 }
