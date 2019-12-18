55728357d113bee0de9b9a2ac2606e994fd62fb2
Jacob <jacobshorterivey@gmail.com>
2019-12-18 15:25:30 -0700

    mapped pet and trait entities

--- JPAFurever/src/main/java/com/skilldistillery/furever/entities/Pet.java
+++ JPAFurever/src/main/java/com/skilldistillery/furever/entities/Pet.java
@@ -1,11 +1,15 @@
 package com.skilldistillery.furever.entities;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 
 @Entity
@@ -32,6 +36,12 @@ public class Pet {
 	@JoinColumn(name = "shelter_id")
 	private Shelter shelter;
 
+	@ManyToMany
+	@JoinTable(name="pet_trait",
+    joinColumns=@JoinColumn(name="pet_id"),
+    inverseJoinColumns=@JoinColumn(name="trait_id")
+  )
+	private List<Trait> traits;
 	
 	// CONSTRUCTORS
 	public Pet() {}
@@ -133,6 +143,80 @@ public class Pet {
 		this.shelter = shelter;
 	}
 
+	
+	
+	public List<Trait> getTraits() {
+		return traits;
+	}
+
+	public void setTraits(List<Trait> traits) {
+		this.traits = traits;
+	}
+	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (adopted ? 1231 : 1237);
+		result = prime * result + age;
+		result = prime * result + ((color == null) ? 0 : color.hashCode());
+		result = prime * result + (fixed ? 1231 : 1237);
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((shelter == null) ? 0 : shelter.hashCode());
+		result = prime * result + ((size == null) ? 0 : size.hashCode());
+		result = prime * result + ((specialConditions == null) ? 0 : specialConditions.hashCode());
+		result = prime * result + weight;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pet other = (Pet) obj;
+		if (adopted != other.adopted)
+			return false;
+		if (age != other.age)
+			return false;
+		if (color == null) {
+			if (other.color != null)
+				return false;
+		} else if (!color.equals(other.color))
+			return false;
+		if (fixed != other.fixed)
+			return false;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (shelter == null) {
+			if (other.shelter != null)
+				return false;
+		} else if (!shelter.equals(other.shelter))
+			return false;
+		if (size == null) {
+			if (other.size != null)
+				return false;
+		} else if (!size.equals(other.size))
+			return false;
+		if (specialConditions == null) {
+			if (other.specialConditions != null)
+				return false;
+		} else if (!specialConditions.equals(other.specialConditions))
+			return false;
+		if (weight != other.weight)
+			return false;
+		return true;
+	}
 
 	@Override
 	public String toString() {
--- JPAFurever/src/main/java/com/skilldistillery/furever/entities/Trait.java
+++ JPAFurever/src/main/java/com/skilldistillery/furever/entities/Trait.java
@@ -1,9 +1,12 @@
 package com.skilldistillery.furever.entities;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Trait {
@@ -15,6 +18,9 @@ public class Trait {
 	
 	private String description;
 	
+	@ManyToMany(mappedBy="traits")
+	private List<Pet> pets;
+	
 	// CONSTRUCTORS
 
 	public Trait(int id, String description) {
@@ -23,11 +29,11 @@ public class Trait {
 		this.description = description;
 	}
 	
-	// Getters and Setters, To String
-
 	public Trait() {
 		super();
 	}
+	// Getters and Setters, To String
+
 
 	public int getId() {
 		return id;
@@ -43,6 +49,44 @@ public class Trait {
 
 	public void setDescription(String description) {
 		this.description = description;
+	}
+
+	
+	public List<Pet> getPets() {
+		return pets;
+	}
+
+	public void setPets(List<Pet> pets) {
+		this.pets = pets;
+	}
+
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Trait other = (Trait) obj;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (id != other.id)
+			return false;
+		return true;
 	}
 
 	@Override
