8050b37f9da355c6a961796e2051a97f5cd4e9d7
Samantha Haviland <shaviland@live.com>
2019-12-19 10:44:19 -0700

    making this thing actually usable.

---
+++ FureverFriends/src/main/java/com/skilldistillery/controllers/UserController.java
@@ -0,0 +1,25 @@
+package com.skilldistillery.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.skilldistillery.furever.entities.User;
+import com.skilldistillery.furever.services.UserService;
+
+@RestController
+@RequestMapping("api/user")
+public class UserController {
+	
+	@Autowired private UserService uSvc;
+	
+	@GetMapping
+	public List<User> index(){
+		return uSvc.displayAllUsers();
+		
+	}
+
+}
