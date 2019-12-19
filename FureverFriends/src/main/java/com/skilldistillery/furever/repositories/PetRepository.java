e42dbd927464a3150ad41140035acc4565afb2bb
Andrew Wong <ndrwchkwng@yahoo.com>
2019-12-19 09:44:44 -0700

    PetReposit

--- FureverFriends/src/main/java/com/skilldistillery/furever/repositories/PetRepository.java
+++ FureverFriends/src/main/java/com/skilldistillery/furever/repositories/PetRepository.java
@@ -1,9 +1,13 @@
 package com.skilldistillery.furever.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.skilldistillery.furever.entities.Breed;
 import com.skilldistillery.furever.entities.Pet;
 
 public interface PetRepository extends JpaRepository<Pet, Integer>{
 
+	public List <Pet> SearchByBreed(Breed breed);
 }
