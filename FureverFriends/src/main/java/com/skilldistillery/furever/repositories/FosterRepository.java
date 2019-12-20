97833c8737103a041ee3571d861cd645b183a4dc
David Norris <Norris.David.Dev@gmail.com>
2019-12-20 14:58:23 -0700

    Changes to foster repo to add more find by methods

--- FureverFriends/src/main/java/com/skilldistillery/furever/repositories/FosterRepository.java
+++ FureverFriends/src/main/java/com/skilldistillery/furever/repositories/FosterRepository.java
@@ -1,5 +1,7 @@
 package com.skilldistillery.furever.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.skilldistillery.furever.entities.Foster;
@@ -7,4 +9,9 @@ import com.skilldistillery.furever.entities.User;
 
 public interface FosterRepository extends JpaRepository<Foster, Integer>{
 	public Foster findByUser(User user);
+	public List<Foster> findBySpeciesId(int id);
+	public List<Foster> findByBreedName(int id);
+	public List<Foster> findByTraitId(int id);
+	
+	
 }
