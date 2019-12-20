796534b30ed6fa769a4494f77eade18ad9acaff5
Zak Saylors <zsaylors11@gmail.com>
2019-12-20 11:46:06 -0700

    add navbar stub

--- FureverFriends/src/main/java/com/skilldistillery/furever/repositories/FosterRepository.java
+++ FureverFriends/src/main/java/com/skilldistillery/furever/repositories/FosterRepository.java
@@ -3,7 +3,8 @@ package com.skilldistillery.furever.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.skilldistillery.furever.entities.Foster;
+import com.skilldistillery.furever.entities.User;
 
 public interface FosterRepository extends JpaRepository<Foster, Integer>{
-
+	public Foster findByUser(User user);
 }
