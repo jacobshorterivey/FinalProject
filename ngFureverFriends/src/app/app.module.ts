244328d962e14cd9b367ceb45ea9503c879baa1d
Jacob <jacobshorterivey@gmail.com>
2019-12-20 13:23:40 -0700

    stubbed services

--- ngFureverFriends/src/app/app.module.ts
+++ ngFureverFriends/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -38,7 +39,8 @@ import { UserListComponent } from './components/user-list/user-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ ngFureverFriends/src/app/services/pet.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PetService {
+  baseUrl = 'http://localhost:8087/';
+  // baseUrl = environment.baseUrl;
+  url = this.baseUrl + 'api/pet';
+
+  constructor(private http: HttpClient) { }
+
+  // If you need to, headers can be added to an HTTP Request by using the HttpHeaders object.
+  // it is used ONLY inside of functions
+  // const httpOptions = {
+  //   headers: new HttpHeaders({
+  //     'Content-Type':  'application/json',
+  //     Authorization: 'my-auth-token'
+  //   })
+  // };
+}
