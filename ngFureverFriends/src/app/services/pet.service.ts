1cbe1e4fed21daeb41a7cd46e2507724da7c28cb
Andrew Wong <ndrwchkwng@yahoo.com>
2019-12-20 14:28:07 -0700

    pet service updated

--- ngFureverFriends/src/app/services/pet.service.ts
+++ ngFureverFriends/src/app/services/pet.service.ts
@@ -1,5 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { Pet } from './../models/pet';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs/operators';
+import { throwError, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -19,4 +22,78 @@ export class PetService {
   //     Authorization: 'my-auth-token'
   //   })
   // };
+
+  // Methods
+
+    // Gets
+    index() {
+      const httpOptions = {
+        headers: new HttpHeaders({
+          'X-Requested-With': 'XMLHttpRequest'
+        })
+      };
+      return this.http.get<Pet[]>(this.url, httpOptions).pipe(
+        catchError((err: any) => {
+          console.error(err);
+          return throwError('PetService.index(): Error retrieving pets');
+        })
+      );
+    }
+
+    showOne(id: string) {
+      const httpOptions = {
+        headers: new HttpHeaders({
+        })
+      };
+      return this.http.get<Pet>(this.url + '/' + id, httpOptions).pipe(
+        catchError((err: any) => {
+          console.error(err);
+          return throwError('Pet update error');
+        })
+      );
+    }
+      show(id): Observable<Pet> {
+      return this.http.get<Pet>(this.url + '/' + id);
+    }
+
+     // Create/Post
+  create(data: Pet) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      })
+    };
+    return this.http.post<Pet>(this.url, data, httpOptions)
+    .pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError('create failed');
+      })
+    );
+  }
+
+  // Update
+  update(id: number, data: Pet) {
+    console.log('in update');
+    console.log(data);
+    const httpOptions = { };
+    return this.http.put<Pet>(this.url + '/' + id, data, httpOptions)
+    .pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError('update failed');
+      })
+    );
+  }
+    // Delete
+  destroy(id: number) {
+    const httpOptions = { };
+    return this.http.delete<Pet>(this.url + '/' + id, httpOptions)
+    .pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError('update failed');
+      })
+    );
+  }
 }
