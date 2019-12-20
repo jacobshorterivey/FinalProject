46f6a7a7a77d13ef7fc5cc3b40c9d4bc9f52e7ed
Andrew Wong <ndrwchkwng@yahoo.com>
2019-12-20 11:21:28 -0700

    ng models fosterToShelter

--- ngFureverFriends/src/app/models/foster.ts
+++ ngFureverFriends/src/app/models/foster.ts
@@ -1,2 +1,12 @@
 export class Foster {
+
+
+  id: number;
+  maxFoster: number;
+
+  // Constructors
+  constructor(id?: number, maxFoster?: number) {
+    this.id = id;
+    this.maxFoster = maxFoster;
+  }
 }
--- ngFureverFriends/src/app/models/image.ts
+++ ngFureverFriends/src/app/models/image.ts
@@ -1,2 +1,13 @@
-export class Image {
+export class image {
+
+
+  id: number;
+  imageUrl: string;
+
+  // Constructor
+
+  constructor(id?: number, imageUrl?: string) {
+    this.id = id;
+    this.imageUrl = imageUrl;
+  }
 }
