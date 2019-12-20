def3aed307427aaa76cd8ce4a39142cf065914e8
David Norris <Norris.David.Dev@gmail.com>
2019-12-20 10:49:12 -0700

    Adding fields and constructor for models

--- ngFureverFriends/src/app/models/species.ts
+++ ngFureverFriends/src/app/models/species.ts
@@ -1,2 +1,12 @@
 export class Species {
+
+  // FIELDS
+  private id: number;
+  private name: string;
+
+  // CONSTRUCTOR
+  constructor(id?: number, name?: string) {
+    this.id = id;
+    this.name = name;
+  }
 }
