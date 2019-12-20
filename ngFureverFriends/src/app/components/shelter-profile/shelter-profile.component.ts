168b317a3cd42c0288a5a0b5028c1ac32cd01bcc
Jacob <jacobshorterivey@gmail.com>
2019-12-20 10:09:10 -0700

    created initial component stubs

---
+++ ngFureverFriends/src/app/components/shelter-profile/shelter-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shelter-profile',
+  templateUrl: './shelter-profile.component.html',
+  styleUrls: ['./shelter-profile.component.css']
+})
+export class ShelterProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
