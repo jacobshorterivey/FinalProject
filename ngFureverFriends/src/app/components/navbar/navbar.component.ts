a8d026648111f609a4ccdd12d45b97f1315afeb1
Zak Saylors <zsaylors11@gmail.com>
2019-12-22 07:39:23 -0700

    login button in nav

--- ngFureverFriends/src/app/components/navbar/navbar.component.ts
+++ ngFureverFriends/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
+import { AuthService } from 'src/app/services/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,11 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth: AuthService, router: Router) { }
 
   navbar = true;
 
   ngOnInit() {
   }
 
+  login(username: string, password: string) {
+    this.auth.login(username, password).subscribe(
+      next => {
+        console.log('RegisterComponent.register(): user logged in, routing to /todo.');
+        this.router.navigateByUrl('/todo');
+      },
+      error => {
+        console.error('RegisterComponent.register(): error logging in.');
+      }
+    );
+  }
 }
