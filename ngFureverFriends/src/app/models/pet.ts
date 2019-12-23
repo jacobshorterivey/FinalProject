2a7448a0b84858757168dffcb4c57e6752701dc3
Samantha Haviland <shaviland@live.com>
2019-12-23 09:26:13 -0700

    stuff

--- ngFureverFriends/src/app/models/pet.ts
+++ ngFureverFriends/src/app/models/pet.ts
@@ -1,7 +1,11 @@
+
+import { Breed } from './breed';
+import { Shelter } from './shelter';
+import { Image } from './image';
+import { Trait } from './trait';
 import { Image } from './image';
 import { Trait } from './trait';
 import { Shelter } from 'src/app/models/shelter';
-import { Breed } from './breed';
 
 export class Pet {
 
@@ -16,14 +20,16 @@ export class Pet {
   specialConditions: string;
   shelter: Shelter;
   breed: Breed;
+
   traits: Trait[];
   images: Image[];
 
   constructor(id?: number, color?: string,
               name?: string, size?: string, age?: number, weight?: number,
               adopted?: boolean, fixed?: boolean, specialConditions?: string,
-              shelter?: Shelter, breed?: Breed, traits?: Trait[], images?: Image[]
-              ) {
+
+              shelter?: Shelter, breed?: Breed, trait?: Trait[], image?: Image[]) {
+
     this.id = id;
     this.color = color;
     this.name = name;
@@ -37,6 +43,7 @@ export class Pet {
     this.breed = breed;
     this.traits = traits;
     this.images = images;
+
   }
 
 }
