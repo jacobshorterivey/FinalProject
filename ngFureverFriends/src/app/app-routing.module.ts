af0f4733cc26498a10e58c021dcfe1c84604ecb4
Zak Saylors <zsaylors11@gmail.com>
2019-12-23 09:48:16 -0700

    updarte to routing.

--- ngFureverFriends/src/app/app-routing.module.ts
+++ ngFureverFriends/src/app/app-routing.module.ts
@@ -24,7 +24,7 @@ const routes: Routes = [
   {path: 'shelternav', component: ShelternavComponent},
   {path: 'pet', component: PetProfileComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'shelterPet', component: PetSearchComponent},
+  {path: 'petsearch', component: PetSearchComponent},
   {path: '**', component: NotFoundComponent}
   ];
 
