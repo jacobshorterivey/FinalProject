59deef70fcc72c856fe67f89747fcd760f639465
Zak Saylors <zsaylors11@gmail.com>
2019-12-27 15:41:22 -0500

    adds shelter nav to shelter profile page when user is logged in.  Needs to check for role and id.

--- ngFureverFriends/src/app/components/shelternav/shelternav.component.ts
+++ ngFureverFriends/src/app/components/shelternav/shelternav.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from 'src/app/services/auth.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShelternavComponent implements OnInit {
 
-  constructor() { }
+  isUserLoggedIn: boolean;
+
+  constructor(private auth: AuthService) { }
 
   ngOnInit() {
+    this.checkLogin();
+  }
+
+  checkLogin() {
+    if (this.auth.checkLogin() === true) {
+      this.isUserLoggedIn = true;
+    }
   }
 
 }
